54c5bdf0b2759605ee4b421a61f105dbbed2daed
Ben Burnet <b7tburnet@gmail.com>
2023-07-08 11:00:26 -0400

    d

--- data_analyze/player_batting_season.py
+++ data_analyze/player_batting_season.py
@@ -300,7 +300,7 @@ st.subheader('Data')
 grid_table = AgGrid(df_short,
                    gridOptions=gridOptions,
                    fit_columns_on_grid_load=True,
-                   height=500,
+                   height=550,
                    width='100%',
                    theme="streamlit",
                    update_mode=GridUpdateMode.GRID_CHANGED,
