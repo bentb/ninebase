c74573ea5fc0b770834d0667b830e794a3c93e45
Ben Burnet <b7tburnet@gmail.com>
2023-07-09 15:23:15 -0400

    'd'

--- pages/player_batting_season.py
+++ pages/player_batting_season.py
@@ -142,7 +142,7 @@ fig_11 = px.box(
     points="all",
     hover_name = "Name",
     height = 500,
-    width = 350,
+    width = 500,
 )
 
 
@@ -156,7 +156,7 @@ fig_12 = px.box(
     points="all",
     hover_name = "Name",
     height = 500,
-    width = 350,
+    width = 500,
 )
 
 
@@ -170,7 +170,7 @@ fig_13 = px.box(
     points="all",
     hover_name = "Name",
     height = 500,
-    width = 350,
+    width = 500,
 )
 
 
@@ -186,7 +186,7 @@ fig_21 = px.box(
     points="all",
     hover_name = "Name",
     height = 500,
-    width = 350,
+    width = 500,
 )
 
 
@@ -200,7 +200,7 @@ fig_22 = px.box(
     points="all",
     hover_name = "Name",
     height = 500,
-    width = 350,
+    width = 500,
 )
 
 
@@ -214,7 +214,7 @@ fig_23 = px.box(
     points="all",
     hover_name = "Name",
     height = 500,
-    width = 350,
+    width = 500,
 )
 
 
