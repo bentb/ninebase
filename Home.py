0a8dc96e951467b65499d93866255a20a1c59509
Ben Burnet <b7tburnet@gmail.com>
2023-07-11 19:04:50 -0400

    'd'

--- Home.py
+++ Home.py
@@ -29,7 +29,7 @@ from st_pages import show_pages_from_config, add_page_title
 # app to add indentation in the sidebar
 add_page_title("")
 
-show_pages_from_config()
+get_pages_from_config()
 
 
 # In[ ]:
@@ -65,54 +65,61 @@ def add_logo():
 add_logo()
 
 
-# from datetime import datetime
-# 
-# def countdown_to_playoffs():
-#     # Get the current date
-#     now = datetime.now().date()
-# 
-#     # Set the target date as October 3rd
-#     target_date = datetime(now.year, 10, 3).date()
-# 
-#     # Calculate the remaining days
-#     remaining_days = (target_date - now).days
-# 
-#     # Customize the countdown style
-#     countdown_style = f"<p style='text-align: right; color: #164f5e; font-size: 18px; padding: 0; margin: 0;'>Days until playoffs: <span style='font-size: 32px;'>{remaining_days}</span></p>"
-# 
-#     # Display the countdown
-#     st.markdown(countdown_style, unsafe_allow_html=True)
-# 
+# In[ ]:
 
-# st.markdown(
-#     """
-#     <style>
-#         body {
-#             display: flex;
-#             flex-direction: row;
-#             justify-content: center;
-#             align-items: flex-start;
-#             height: 100vh;
-#             margin: 0;
-#         }
-# 
-#         .title {
-#             font-size: 46px;
-#             font-weight: bold;
-#             margin-bottom: 6px;
-#             font-family: Lato, sans-serif;
-#             color: #164f5e;
-#         }
-# 
-#         .description {
-#             font-size: 24px;
-#             margin-bottom: 5px;
-#             font-family: Lato, sans-serif;
-#         }
-#     </style>
-#     """,
-#     unsafe_allow_html=True
-# )
+
+from datetime import datetime
+
+def countdown_to_playoffs():
+    # Get the current date
+    now = datetime.now().date()
+
+    # Set the target date as October 3rd
+    target_date = datetime(now.year, 10, 3).date()
+
+    # Calculate the remaining days
+    remaining_days = (target_date - now).days
+
+    # Customize the countdown style
+    countdown_style = f"<p style='text-align: right; color: #164f5e; font-size: 18px; padding: 0; margin: 0;'>Days until playoffs: <span style='font-size: 32px;'>{remaining_days}</span></p>"
+
+    # Display the countdown
+    st.markdown(countdown_style, unsafe_allow_html=True)
+
+
+# In[ ]:
+
+
+st.markdown(
+    """
+    <style>
+        body {
+            display: flex;
+            flex-direction: row;
+            justify-content: center;
+            align-items: flex-start;
+            height: 100vh;
+            margin: 0;
+        }
+
+        .title {
+            font-size: 46px;
+            font-weight: bold;
+            margin-bottom: 6px;
+            font-family: Lato, sans-serif;
+            color: #164f5e;
+        }
+
+        .description {
+            font-size: 24px;
+            margin-bottom: 5px;
+            font-family: Lato, sans-serif;
+        }
+    </style>
+    """,
+    unsafe_allow_html=True
+)
+
 
 # In[9]:
 
