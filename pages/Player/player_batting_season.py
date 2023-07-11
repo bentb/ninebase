ba3f627c955e226b6511030805ac7a30d7890866
Ben Burnet <b7tburnet@gmail.com>
2023-07-10 23:58:24 -0400

    'd'

--- pages/Player/player_batting_season.py
+++ pages/Player/player_batting_season.py
@@ -58,7 +58,7 @@ from st_pages import show_pages_from_config, add_page_title
 
 # Either this or add_indentation() MUST be called on each page in your
 # app to add indentation in the sidebar
-add_page_title()
+add_page_title("")
 
 show_pages_from_config()
 
