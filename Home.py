9fc6339c91ebc95e5c8cf29755f02b4588a11e0d
Ben Burnet <b7tburnet@gmail.com>
2023-07-10 22:56:14 -0400

    'd'

--- Home.py
+++ Home.py
@@ -21,16 +21,13 @@ st.set_page_config(
 
 from st_pages import Page, Section, show_pages, add_page_title
 
-# Optional -- adds the title and icon to the current page
-add_page_title("Page Title Goes Here")
-
 # Specify what pages should be shown in the sidebar, and what their titles and icons should be
 show_pages(
     [
         Page("Home.py", "Home"),
         Section("Player"),
-        Page("pages/Player/player_batting_season.py", "Batting"),
-        Page("pages/Player/player_pitching_season.py", "Pitching"),
+        Page("pages/Player/player_batting_season.py", "Batting", in_section=True),
+        Page("pages/Player/player_pitching_season.py", "Pitching", in_section=True),
         Section("Team"),
         Section("References"),
         Page("pages/References/Data_Dictionary.py"),
