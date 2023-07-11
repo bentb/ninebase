6ed32a73862d3bcdfa07b1badd79d0f35eef2a64
Ben Burnet <b7tburnet@gmail.com>
2023-07-10 20:42:01 -0400

    'd'

--- pages/Player/player_batting_season.py
+++ pages/Player/player_batting_season.py
@@ -60,13 +60,24 @@ st.set_page_config(
 # In[ ]:
 
 
-### trying st-pages
+from st_pages import Page, Section, show_pages, add_page_title
 
+# Optional -- adds the title and icon to the current page
+add_page_title("Page Title Goes Here")
 
-# In[1]:
-
-
-
+# Specify what pages should be shown in the sidebar, and what their titles and icons
+# should be
+show_pages(
+    [
+        Page("Home.py", "Home"),
+        Section("Player"),
+            Page("pages/Player/player_batting_season.py", "Batting"),
+            Page("pages/Player/player_pitching_season.py", "Pitching"),
+        Section("Team"),
+        Section("References"),
+            Page("pages/References/Data_Dictionary.py"),
+    ]
+)
 
 
 # In[ ]:
