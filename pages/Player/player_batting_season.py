b1c929c3782686ec80c7cab041e3cec63bbb2201
Ben Burnet <b7tburnet@gmail.com>
2023-07-10 22:59:25 -0400

    'd'

--- pages/Player/player_batting_season.py
+++ pages/Player/player_batting_season.py
@@ -50,21 +50,6 @@ st.set_page_config(
     }
 )
 
-from st_pages import Page, Section, show_pages, add_page_title
-
-# Specify what pages should be shown in the sidebar, and what their titles and icons should be
-show_pages(
-    [
-        Page("Home.py", "Home"),
-        Section("Player"),
-        Page("pages/Player/player_batting_season.py", "Batting"),
-        Page("pages/Player/player_pitching_season.py", "Pitching"),
-        Section("Team"),
-        Section("References"),
-        Page("pages/References/Data_Dictionary.py"),
-    ]
-)
-
 
 # In[83]:
 
